86e938e4f8a6630a0fa4d0a4b5dcda89a8b250ce
henriktoth <henriktoth0517@gmail.com>
2025-08-07 13:11:08 +0200

    feat: added responsivity in navbar with hamburger menu

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer(){
     return(
         <div className="flex justify-between bg-amber-400 text-white p-4">
-            <p>&copy; 2023 burger.CO. All rights reserved.</p>
+            <p>&copy; {new Date().getFullYear()} burger.CO. All rights reserved.</p>
         </div>
     )
 }
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -14,11 +14,32 @@ function Navbar() {
         <h1 className="text-3xl font-bold">burger.CO</h1>
       </div>
       {/* Navigation */}
-      <div className="flex items-center ">
-        <NavItem href="" title="Menü" />
-        <NavItem href="" title="Éttermek" />
-        <NavItem href="" title="Kapcsolat" />
-        <ShoppingCart />
+      <div className="flex items-center">
+        {/* Desktop Navigation */}
+        <div className="hidden md:flex items-center">
+          <NavItem href="" title="Menü" />
+          <NavItem href="" title="Éttermek" />
+          <NavItem href="" title="Kapcsolat" />
+          <ShoppingCart />
+        </div>
+        {/* Mobile Hamburger Menu */}
+        <div className="md:hidden flex items-center">
+          <button className="text-white focus:outline-none">
+            <svg 
+              className="w-10 h-10" 
+              fill="none" 
+              stroke="white" 
+              viewBox="0 0 24 24"
+            >
+              <path 
+              strokeLinecap="round" 
+              strokeLinejoin="round" 
+              strokeWidth={3} 
+              d="M4 6h16M4 12h16M4 18h16" 
+              />
+            </svg>
+          </button>
+        </div>
       </div>      
     </div>
   )
