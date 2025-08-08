d56aaf3ce5304a42584911008a6abcbbd4192996
henriktoth <henriktoth0517@gmail.com>
2025-08-08 19:13:15 +0200

    fix: deleted useState for now

--- src/components/Navbar/ShoppingCart.tsx
+++ src/components/Navbar/ShoppingCart.tsx
@@ -1,11 +1,8 @@
-import { useState } from 'react';
-
 function ShoppingCart() {
-    const [itemCount, setItemCount] = useState(0);
 
     return(
         <div className="mx-5 cursor-pointer">
-            <p className="bg-amber-50 rounded-full max-w-fit text-amber-400 px-2">{itemCount}</p>
+            <p className="bg-amber-50 rounded-full max-w-fit text-amber-400 px-2">0</p>
             <img 
             src="/navbar/ShoppingCartIcon.svg" 
             alt="Shopping Cart" 
