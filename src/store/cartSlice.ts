0b18f1a7be470be228b836007176206e06074866
henriktoth <henriktoth0517@gmail.com>
2025-09-05 18:31:35 +0200

    feat: added provider

--- src/store/cartSlice.ts
+++ src/store/cartSlice.ts
@@ -1,6 +1,12 @@
 import {createSlice} from '@reduxjs/toolkit';
 
-const initialState: { name: string; ingredients: string; allergens: string[]; price: number; count: number }[] = [];
+const initialState: { 
+    name: string; 
+    ingredients: string; 
+    allergens: string[]; 
+    price: number; 
+    count: number 
+}[] = [];
 
 const cartSlice = createSlice({
     name : "cart",
@@ -9,9 +15,21 @@ const cartSlice = createSlice({
         addToCart: (state, action) => {
             state.push(action.payload);
         },
-    }
+        removeFromCart: (state, action) => {
+            const index = state.findIndex(item => item.name === action.payload.name);
+            if (index !== -1) {
+                state.splice(index, 1);
+            }
+        },
+        updateItemCount: (state, action) => {
+            const index = state.findIndex(item => item.name === action.payload.name);
+            if (index !== -1) {
+                state[index].count = action.payload.count;
+            }
+        },
+    },
 })
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart, updateItemCount } = cartSlice.actions;
 
 export default cartSlice.reducer;
